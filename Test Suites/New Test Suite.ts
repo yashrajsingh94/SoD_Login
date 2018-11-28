e221316c8cf3ea6a4e2f4bce72bec758336dd288
yashrajs <yashrajs@KAI-DT161.adventurena.com>
2018-11-28 19:01:15 +0530

    msg

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-11-28T18:51:37</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>818d49eb-4b71-405e-ae64-cd22210abb7b</testSuiteGuid>
+   <testCaseLink>
+      <guid>f32f13b3-268f-4096-b294-14dab6faec32</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LaunchBrowser/Browser</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
